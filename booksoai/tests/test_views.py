b4d7048bb857efffe206bbee72178046d32651aa
Rafael Novello <rafa.reis.novello@gmail.com>
2014-02-17 15:30:31 -0300

    Implementados verbos GetRecord e ListRecords e mecanismo de Selective Harvesting. #4

--- booksoai/tests/test_views.py
+++ booksoai/tests/test_views.py
@@ -1,8 +1,10 @@
 import unittest
 
+from bson import json_util
 from pyramid import testing
 from pyramid.httpexceptions import HTTPNotFound
 
+from booksoai.views import oai_pmh
 from booksoai.utils import get_db_connection
 
 settings = {}
@@ -12,6 +14,20 @@ settings['db_conn'] = get_db_connection(settings)
 
 
 class ViewTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        db = settings['db_conn']
+        with open('booksoai/tests/fixtures/books.bson') as fixture:
+            for line in fixture:
+                book = json_util.loads(line)
+                db.books.insert(book)
+
+    @classmethod
+    def tearDownClass(cls):
+        db = settings['db_conn']
+        db.connection.drop_database(db.name)
+
     def setUp(self):
         self.config = testing.setUp()
 
@@ -19,15 +35,46 @@ class ViewTests(unittest.TestCase):
         testing.tearDown()
 
     def test_invalid_verb_return_404(self):
-        from booksoai.views import oai_pmh
         request = testing.DummyRequest()
         request.params = {'verb':'bla'}
         self.assertRaises(HTTPNotFound, oai_pmh, request)
 
-    # def test_dummy_identify_return_identify_info(self):
-    #     from booksoai.views import oai_pmh
-    #     request = testing.DummyRequest()
-    #     request.params = {'verb':'Identify'}
-    #     response = oai_pmh(request)
-    #     self.assertEqual(response['OAI-PMH']['Identify']['repositoryName'], 'SciELO Books')
+    def test_get_record_with_identifier(self):
+        request = testing.DummyRequest()
+        request.db = settings['db_conn']
+        request.params = {'verb': 'GetRecord', 'identifier': '37t'}
+        resp = oai_pmh(request)
+        resp = str(resp)
+        self.assertIn('<identifier>37t</identifier>', resp)
+        self.assertIn(
+        '<request verb="GetRecord" identifier="37t">http://books.scielo.org/oai/</request>',
+        resp)
+        self.assertEqual(resp.count('<record>'), 1)
+        
+    def test_list_records_with_from(self):
+        request = testing.DummyRequest()
+        request.db = settings['db_conn']
+        request.params = {'verb': 'ListRecords', 'from': '2014-02-04'}
+        resp = oai_pmh(request)
+        resp = str(resp)
+        self.assertEqual(resp.count('<record>'), 2)
+        self.assertIn('<datestamp>2014-02-04</datestamp>', resp)
+        self.assertIn('<datestamp>2014-02-05</datestamp>', resp)
 
+    def test_list_records_with_from_and_until(self):
+        request = testing.DummyRequest()
+        request.db = settings['db_conn']
+        request.params = {'verb': 'ListRecords', 'from': '2014-02-04', 'until': '2014-02-04'}
+        resp = oai_pmh(request)
+        resp = str(resp)
+        self.assertEqual(resp.count('<record>'), 1)
+        self.assertIn('<datestamp>2014-02-04</datestamp>', resp)
+
+    def test_list_records_with_set(self):
+        request = testing.DummyRequest()
+        request.db = settings['db_conn']
+        request.params = {'verb': 'ListRecords', 'set': 'bla-x-ble'}
+        resp = oai_pmh(request)
+        resp = str(resp)
+        self.assertEqual(resp.count('<record>'), 1)
+        self.assertIn('<setSpec>bla-x-ble</setSpec>', resp)
--- booksoai/views.py
+++ booksoai/views.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
+import re
 import oaipmh
+
+from datetime import datetime
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPNotFound
 
@@ -10,6 +13,8 @@ VERBS = {
     'ListMetadataFormats': (oaipmh.ListMetadataFormatsVerb, False),
     'ListIdentifiers': (oaipmh.ListIdentifiersVerb, True),
     'ListSets': (oaipmh.ListSetsVerb, True),
+    'GetRecord': (oaipmh.GetRecordVerb, True),
+    'ListRecords': (oaipmh.ListRecordsVerb, True),
 }
 
 @view_config(route_name='oai_pmh', renderer='oai')
@@ -20,7 +25,28 @@ def oai_pmh(request):
     except KeyError:
         raise HTTPNotFound()
 
-    params = {'books': request.db.books.find()} if need_books else {}
+    params = request.params.copy()
+    if need_books:
+        search = {}
+        if 'identifier' in params:
+            search['identifier'] = params['identifier']
+
+        if 'set' in params:
+            _set = params['set']
+            _set = _set.replace('-', ' ')
+            search['publisher'] = re.compile(_set, re.IGNORECASE)
+
+        if 'from' in params:
+            _from = params['from']
+            _from = datetime.strptime(_from, '%Y-%m-%d')
+            search['datestamp'] = {'$gte': _from}
+
+        if 'until' in params:
+            until = params['until']
+            until = datetime.strptime(until, '%Y-%m-%d')
+            search.setdefault('datestamp', {})['$lte'] = until
+
+        params['books'] = request.db.books.find(search)
 
     return OaiVerb(**params)
 
